a3246477cbd3fac15300cb1bced1c22ab5d59705
dev656 <devenderaspldelhi@gmail.com>
2020-09-15 14:48:31 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/bhumika/big/bhumikabigshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bhumika.big.bhumikabigshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/base/libraryExport.dart
@@ -0,0 +1,91 @@
+export 'package:bhumikabigshop/app/ApiAdmin.dart';
+export 'package:bhumikabigshop/app/ApiClient.dart';
+export 'package:bhumikabigshop/app/HttpClient.dart';
+export 'package:bhumikabigshop/app/AppConstants.dart';
+export 'package:bhumikabigshop/app/AppPreferences.dart';
+export 'package:bhumikabigshop/app/ServiceLocator.dart';
+
+export 'package:bhumikabigshop/modal/CartSummery.dart';
+export 'package:bhumikabigshop/modal/ProductDetails.dart';
+export 'package:bhumikabigshop/modal/KonnectDetails.dart';
+export 'package:bhumikabigshop/modal/UserAdmin.dart';
+export 'package:bhumikabigshop/modal/UserProfile.dart';
+export 'package:bhumikabigshop/modal/UserLoginType.dart';
+
+export 'package:bhumikabigshop/ui/admin/AdminDashboard.dart';
+export 'package:bhumikabigshop/ui/admin/AdminLedger.dart';
+export 'package:bhumikabigshop/ui/admin/AdminPayReceipt.dart';
+export 'package:bhumikabigshop/ui/admin/AdminProformaInvoice.dart';
+export 'package:bhumikabigshop/ui/admin/AdminPurchaseInvoice.dart';
+export 'package:bhumikabigshop/ui/admin/AdminSalesInvoice.dart';
+export 'package:bhumikabigshop/ui/admin/AdminPartyMaster.dart';
+export 'package:bhumikabigshop/ui/admin/AdminSalesOrder.dart';
+
+export 'package:bhumikabigshop/utils/CarouselSlider.dart';
+export 'package:bhumikabigshop/utils/DashboardButton.dart';
+export 'package:bhumikabigshop/utils/GroupedCheckbox.dart';
+export 'package:bhumikabigshop/utils/KonnectButton.dart';
+export 'package:bhumikabigshop/utils/PinchZoomImage.dart';
+export 'package:bhumikabigshop/utils/RadioButtonGroup.dart';
+
+export 'package:bhumikabigshop/ui/AdminLogin.dart';
+export 'package:bhumikabigshop/ui/AboutScreen.dart';
+export 'package:bhumikabigshop/ui/BankingScreen.dart';
+export 'package:bhumikabigshop/ui/ContactScreen.dart';
+export 'package:bhumikabigshop/ui/DashboardScreen.dart';
+export 'package:bhumikabigshop/ui/GalleryScreen.dart';
+export 'package:bhumikabigshop/ui/LocationScreen.dart';
+export 'package:bhumikabigshop/ui/AdminLogin.dart';
+export 'package:bhumikabigshop/ui/OffersScreen.dart';
+export 'package:bhumikabigshop/ui/RegistrationScreen.dart';
+export 'package:bhumikabigshop/ui/SupportScreen.dart';
+export 'package:bhumikabigshop/ui/WebStoreScreen.dart';
+
+export 'package:bhumikabigshop/ui/link/LinkUserDashboard.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserLedger.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserPartyMaster.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserPayReceipt.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserProformaInvoice.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserSalesOrder.dart';
+export 'package:bhumikabigshop/ui/link/LinkUserSalesInvoice.dart';
+
+export 'package:bhumikabigshop/ui/pm/PartyMasterAddPayment.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterDashboard.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterInvoice.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterLedger.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterPaymentQr.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterPayment.dart';
+export 'package:bhumikabigshop/ui/pm/PartyMasterSalesOrder.dart';
+
+export 'package:bhumikabigshop/ui/pm/profile/PartyMasterForgot.dart';
+export 'package:bhumikabigshop/ui/pm/profile/PartyMasterLogin.dart';
+export 'package:bhumikabigshop/ui/pm/profile/PartyMasterMobile.dart';
+export 'package:bhumikabigshop/ui/pm/profile/PartyMasterRegister.dart';
+export 'package:bhumikabigshop/ui/pm/profile/PartyMasterUpdate.dart';
+
+export 'package:bhumikabigshop/ui/store/OrderCart.dart';
+export 'package:bhumikabigshop/ui/store/OrderSummery.dart';
+export 'package:bhumikabigshop/ui/store/ProductDetail.dart';
+export 'package:bhumikabigshop/ui/store/ProductList.dart';
+export 'package:bhumikabigshop/ui/store/ProductSearch.dart';
+export 'package:bhumikabigshop/ui/store/WebViewFlutter.dart';
+
+export 'package:bhumikabigshop/ui/view/LedgerView.dart';
+export 'package:bhumikabigshop/ui/view/PartyMasterView.dart';
+export 'package:bhumikabigshop/ui/view/PayReceiptView.dart';
+export 'package:bhumikabigshop/ui/view/ProformaInvoiceView.dart';
+export 'package:bhumikabigshop/ui/view/PurchaseInvoiceView.dart';
+export 'package:bhumikabigshop/ui/view/SalesInvoiceView.dart';
+export 'package:bhumikabigshop/ui/view/SalesOrderView.dart';
+
+export 'dart:convert';
+export '../../main.dart';
+export 'package:flutter/services.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/rendering.dart';
+export 'package:getflutter/getflutter.dart';
+export 'package:qr_flutter/qr_flutter.dart';
+export 'package:flutter_html/flutter_html.dart';
+export 'package:awesome_dialog/awesome_dialog.dart';
+export 'package:progress_dialog/progress_dialog.dart';
+export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
